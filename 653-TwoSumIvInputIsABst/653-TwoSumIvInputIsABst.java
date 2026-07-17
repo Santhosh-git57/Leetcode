ffbc00ee5699fff8d6ac48af9d19771620e8fb28
Santhosh <santhoshsk28677@gmail.com>
2026-07-17 15:03:44 +0530

    Create 653-TwoSumIvInputIsABst

---
+++ 653-TwoSumIvInputIsABst/653-TwoSumIvInputIsABst.java
@@ -0,0 +1,18 @@
+// Last updated: 7/17/2026, 3:03:44 PM
+class Solution {
+    HashSet<Integer> set = new HashSet<>();
+
+    public boolean findTarget(TreeNode root, int k) {
+        // Base case: null node
+        if (root == null) return false;
+
+        // If complement is found, return true
+        if (set.contains(k - root.val)) return true;
+
+        // Otherwise, add current node value to the set
+        set.add(root.val);
+
+        // Recurse on left and right subtrees
+        return findTarget(root.left, k) || findTarget(root.right, k);
+    }
+}
