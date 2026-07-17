e4fb5878a88bea31a27fb2036fa741c6a646bf20
Santhosh <santhoshsk28677@gmail.com>
2026-07-17 15:03:31 +0530

    Create 975-RangeSumOfBst

---
+++ 975-RangeSumOfBst/975-RangeSumOfBst.java
@@ -0,0 +1,15 @@
+// Last updated: 7/17/2026, 3:03:31 PM
+public class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        int currentVal = (root.val >= low && root.val <= high) ? root.val : 0;
+
+        int leftSum = rangeSumBST(root.left, low, high);
+        int rightSum = rangeSumBST(root.right, low, high);
+
+        return currentVal + leftSum + rightSum;
+    }
+}
