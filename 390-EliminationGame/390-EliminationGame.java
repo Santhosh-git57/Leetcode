b831002a98e7f372484ddcd2562ceffc67607a37
Santhosh <santhoshsk28677@gmail.com>
2026-07-17 15:04:28 +0530

    Create 390-EliminationGame

---
+++ 390-EliminationGame/390-EliminationGame.java
@@ -0,0 +1,18 @@
+// Last updated: 7/17/2026, 3:04:29 PM
+class Solution {
+
+    public int lastRemaining(int n) {
+        return fun(n, true);
+    }
+
+    private int fun(int n, boolean l2r) {
+        if (n == 1) return 1;
+
+        if (l2r || n % 2 == 1) {
+            return 2 * fun(n / 2, !l2r);
+        }
+        else {
+            return 2 * fun(n / 2, !l2r) - 1;
+        }
+    }
+}
