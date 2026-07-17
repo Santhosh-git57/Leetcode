488bf6639847b7b71d42059c372b627ba67d352d
Santhosh <santhoshsk28677@gmail.com>
2026-07-17 15:03:37 +0530

    Create 807-CustomSortString

---
+++ 807-CustomSortString/807-CustomSortString.java
@@ -0,0 +1,22 @@
+// Last updated: 7/17/2026, 3:03:37 PM
+import java.util.HashMap;
+
+class Solution {
+    public String customSortString(String order, String s) {
+        StringBuilder result = new StringBuilder();
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+        }
+        for (char c : order.toCharArray()) {
+            if (mp.containsKey(c)) {
+                result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+                mp.remove(c);
+            }
+        }
+        for (char c : mp.keySet()) {
+            result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+        }
+        return result.toString();
+    }
+}
