bffcbb3d2579b922dad7813c61d8518d0dbcf22b
Santhosh <santhoshsk28677@gmail.com>
2026-08-22 09:41:45 +0530

    File updated at 8/22/2026, 9:41:45 AM

--- 258-AddDigits/258-AddDigits.java
+++ 258-AddDigits/258-AddDigits.java
@@ -1,23 +1,10 @@
-// Last updated: 8/22/2026, 9:41:22 AM
+// Last updated: 8/22/2026, 9:41:45 AM
 1class Solution {
-2    public void moveZeroes(int[] nums) {
-3        if (nums.length == 1) {
-4            return;
-5        }
-6
-7        int nonZeroIndex = 0;
-8
-9        for (int i = 0; i < nums.length; i++) {
-10            if (nums[i] != 0) {
-11                nums[nonZeroIndex] = nums[i];
-12                nonZeroIndex++;
-13            }
-14        }
-15
-16       
-17         while(nonZeroIndex<nums.length){
-18            nums[nonZeroIndex] = 0;
-19            nonZeroIndex++;
-20        }
-21    }
-22}
+2    public boolean canWinNim(int n) {
+3        
+4        if (n % 4 != 0)
+5            return true;
+6        else
+7            return false;
+8    }
+9}
