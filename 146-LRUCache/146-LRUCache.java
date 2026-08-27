cf7719b444421427a13434a57b3a8fa27e18aa29
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:13:30 +0530

    File updated at 8/27/2026, 9:13:30 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,17 +1,14 @@
-// Last updated: 8/27/2026, 9:13:07 AM
+// Last updated: 8/27/2026, 9:13:30 AM
 1class Solution {
-2  public List<Integer> findDisappearedNumbers(int[] nums) {
-3    List<Integer> ans = new ArrayList<>();
-4
-5    for (final int num : nums) {
-6      final int index = Math.abs(num) - 1;
-7      nums[index] = -Math.abs(nums[index]);
-8    }
-9
-10    for (int i = 0; i < nums.length; ++i)
-11      if (nums[i] > 0)
-12        ans.add(i + 1);
-13
-14    return ans;
-15  }
-16}
+2    public int findMinArrowShots(int[][] segments) {
+3        Arrays.sort(segments, (a, b) -> Integer.compare(a[1], b[1]));
+4        int ans = 0, arrow = 0;
+5        for (int i = 0; i < segments.length; i ++) {
+6            if (ans == 0 || segments[i][0] > arrow) {
+7                ans ++;
+8                arrow = segments[i][1];
+9            }
+10        }
+11        return ans;
+12    }
+13}
