14c760cc30042166c5804a734a5788e68d92cd11
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:08:59 +0530

    File updated at 8/27/2026, 9:08:59 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,16 +1,29 @@
-// Last updated: 8/27/2026, 9:08:32 AM
-1class Solution {
-2    public char findTheDifference(String s, String t) {
-3        // Intuition: sort both strings and find first mismatch
-4        char[] sChars = s.toCharArray();
-5        char[] tChars = t.toCharArray();
-6        Arrays.sort(sChars);
-7        Arrays.sort(tChars);
-8
-9        int i = 0;
-10        while (i < sChars.length && sChars[i] == tChars[i]) {
-11            i++;
-12        }
-13        return tChars[i];
-14    }
-15}
+// Last updated: 8/27/2026, 9:08:59 AM
+1public class Solution {
+2    public int longestPalindrome(String s) {
+3        // Initialize a set to keep track of characters with odd frequencies
+4        HashSet<Character> charSet = new HashSet<>();
+5        // Initialize the length of the longest palindrome
+6        int length = 0;
+7        
+8        // Iterate over each character in the string
+9        for (char c : s.toCharArray()) {
+10            // If the character is already in the set, remove it and increase the length by 2
+11            if (charSet.contains(c)) {
+12                charSet.remove(c);
+13                length += 2;
+14            } else {
+15                // If the character is not in the set, add it to the set
+16                charSet.add(c);
+17            }
+18        }
+19        
+20        // If there are any characters left in the set, add 1 to the length for the middle character
+21        if (!charSet.isEmpty()) {
+22            length += 1;
+23        }
+24        
+25        // Return the total length of the longest palindrome
+26        return length;
+27    }
+28}
