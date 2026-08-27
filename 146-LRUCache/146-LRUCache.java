241e3f1dab010cfa50e9cbb14c5c313062a014c2
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:12:27 +0530

    File updated at 8/27/2026, 9:12:27 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,29 +1,21 @@
-// Last updated: 8/27/2026, 9:12:08 AM
+// Last updated: 8/27/2026, 9:12:27 AM
 1class Solution {
-2    public int thirdMax(int[] nums) {
-3        long max = Long.MIN_VALUE;
-4        long secondMax = Long.MIN_VALUE;
-5        long thirdMax = Long.MIN_VALUE;
-6
-7        for (int num : nums) {
-8            long val = num;
-9
-10            if (val == max || val == secondMax || val == thirdMax) {
-11                continue;
-12            }
-13
-14            if (val > max) {
-15                thirdMax = secondMax;
-16                secondMax = max;
-17                max = val;
-18            } else if (val > secondMax) {
-19                thirdMax = secondMax;
-20                secondMax = val;
-21            } else if (val > thirdMax) {
-22                thirdMax = val;
-23            }
-24        }
-25
-26        return thirdMax == Long.MIN_VALUE ? (int) max : (int) thirdMax;
-27    }
-28}
+2    public int arrangeCoins(int n) {
+3        int low=1;
+4        int high=n;
+5        int ans=1;
+6        while(low<=high){
+7            int mid=low+(high-low)/2;
+8            if(helper(mid)<=n){
+9                ans=mid;
+10                low=mid+1;
+11            }
+12            else high=mid-1;
+13        }
+14        return ans;
+15    }
+16    public long helper(int mid){
+17        long sum=(long)mid*(mid+1)/2;
+18        return sum;
+19    }
+20}
