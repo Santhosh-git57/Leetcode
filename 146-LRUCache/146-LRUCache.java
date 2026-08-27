12e5200f0e7c562338aaec300ae391028cd6d6c5
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:10:08 +0530

    File updated at 8/27/2026, 9:10:08 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,29 +1,22 @@
-// Last updated: 8/27/2026, 9:09:47 AM
-1public class Solution {
-2    public int longestPalindrome(String s) {
-3        // Initialize a set to keep track of characters with odd frequencies
-4        HashSet<Character> charSet = new HashSet<>();
-5        // Initialize the length of the longest palindrome
-6        int length = 0;
-7        
-8        // Iterate over each character in the string
-9        for (char c : s.toCharArray()) {
-10            // If the character is already in the set, remove it and increase the length by 2
-11            if (charSet.contains(c)) {
-12                charSet.remove(c);
-13                length += 2;
-14            } else {
-15                // If the character is not in the set, add it to the set
-16                charSet.add(c);
+// Last updated: 8/27/2026, 9:10:08 AM
+1class Solution {
+2    public List<String> fizzBuzz(int n) {
+3        List<String> ans = new ArrayList<>();
+4
+5        for(int i=1; i<=n; i++) {
+6            if(i%3 ==0 && i%5==0) {
+7                ans.add("FizzBuzz");
+8            }
+9            else if(i%3==0) {
+10                ans.add("Fizz");
+11            }
+12            else if(i%5==0) {
+13                ans.add("Buzz");
+14            }
+15            else {
+16                ans.add(Integer.toString(i));
 17            }
 18        }
-19        
-20        // If there are any characters left in the set, add 1 to the length for the middle character
-21        if (!charSet.isEmpty()) {
-22            length += 1;
-23        }
-24        
-25        // Return the total length of the longest palindrome
-26        return length;
-27    }
-28}
+19        return ans;
+20    }
+21}
