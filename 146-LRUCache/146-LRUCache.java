50587c80e7ede504f9487786b5f1b82a2c687981
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:08:32 +0530

    File updated at 8/27/2026, 9:08:32 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,23 +1,16 @@
-// Last updated: 8/27/2026, 9:07:56 AM
+// Last updated: 8/27/2026, 9:08:32 AM
 1class Solution {
-2    public int wiggleMaxLength(int[] nums) {
-3        if (nums.length == 0) return 0;
-4        
-5        int n = nums.length;
-6        if (n < 2) return n;
-7        
-8        int up = 1, down = 1;
-9        
-10        for (int i = 1; i < n; ++i) {
-11            if (nums[i] > nums[i - 1]) {
-12                up = down + 1;
-13            } else if (nums[i] < nums[i - 1]) {
-14                down = up + 1;
-15            }
-16        }
-17        
-18        return Math.max(up, down);
-19    }
-20    
-21    
-22}
+2    public char findTheDifference(String s, String t) {
+3        // Intuition: sort both strings and find first mismatch
+4        char[] sChars = s.toCharArray();
+5        char[] tChars = t.toCharArray();
+6        Arrays.sort(sChars);
+7        Arrays.sort(tChars);
+8
+9        int i = 0;
+10        while (i < sChars.length && sChars[i] == tChars[i]) {
+11            i++;
+12        }
+13        return tChars[i];
+14    }
+15}
