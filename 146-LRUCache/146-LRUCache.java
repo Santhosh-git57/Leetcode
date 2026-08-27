be3e59cf0627df7d364d0f36f2d0501c02cb0b91
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:07:56 +0530

    File updated at 8/27/2026, 9:07:56 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,28 +1,23 @@
-// Last updated: 8/27/2026, 9:07:31 AM
+// Last updated: 8/27/2026, 9:07:56 AM
 1class Solution {
-2    public int[] intersect(int[] nums1, int[] nums2) {
-3        HashMap<Integer, Integer> map = new HashMap<>();
+2    public int wiggleMaxLength(int[] nums) {
+3        if (nums.length == 0) return 0;
 4        
-5        for(int num : nums1){
-6            map.put(num, map.getOrDefault(num, 0) + 1);
-7        }
-8
-9        List<Integer> list = new ArrayList<>();
-10        
-11        for(int elem : nums2){
-12            if(map.getOrDefault(elem, 0) > 0){
-13                list.add(elem);
-14                map.put(elem, map.get(elem) - 1);
+5        int n = nums.length;
+6        if (n < 2) return n;
+7        
+8        int up = 1, down = 1;
+9        
+10        for (int i = 1; i < n; ++i) {
+11            if (nums[i] > nums[i - 1]) {
+12                up = down + 1;
+13            } else if (nums[i] < nums[i - 1]) {
+14                down = up + 1;
 15            }
 16        }
-17
-18        int[] result = new int[list.size()];
-19        int i = 0;
-20        
-21        for(int val : list){
-22            result[i++] = val;
-23        }
-24
-25        return result;
-26    }
-27}
+17        
+18        return Math.max(up, down);
+19    }
+20    
+21    
+22}
