30c944cf36a4a9f1f6d1687cc9f3c041ab028036
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:07:10 +0530

    File updated at 8/27/2026, 9:07:10 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,10 +1,6 @@
-// Last updated: 8/27/2026, 9:06:51 AM
+// Last updated: 8/27/2026, 9:07:10 AM
 1class Solution {
-2    public int[] countBits(int n) {
-3        int[] ans = new int[n + 1];
-4        for (int i = 1; i <= n; i++) {
-5            ans[i] = ans[i >> 1] + (i & 1);
-6        }
-7        return ans;
-8    }
-9}
+2    public boolean isPowerOfFour(int n) {
+3        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+4    }
+5}
