8d176469b5696e17c60cb2761067f57d4d26e79f
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:14:06 +0530

    File updated at 8/27/2026, 9:14:05 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,14 +1,37 @@
-// Last updated: 8/27/2026, 9:13:30 AM
+// Last updated: 8/27/2026, 9:14:05 AM
 1class Solution {
-2    public int findMinArrowShots(int[][] segments) {
-3        Arrays.sort(segments, (a, b) -> Integer.compare(a[1], b[1]));
-4        int ans = 0, arrow = 0;
-5        for (int i = 0; i < segments.length; i ++) {
-6            if (ans == 0 || segments[i][0] > arrow) {
-7                ans ++;
-8                arrow = segments[i][1];
-9            }
-10        }
-11        return ans;
-12    }
-13}
+2    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+3
+4        int count=0;
+5        HashMap<Integer,Integer>map=new HashMap<>();
+6        for(int i=0;i<nums1.length;i++)
+7        {
+8            for(int j=0;j<nums2.length;j++)
+9            {
+10                int sum=nums1[i]+nums2[j];
+11                if(map.containsKey(sum))
+12                {
+13                    int freq=map.get(sum);
+14                    map.put(sum,freq+1);
+15                }
+16                else
+17                {
+18                    map.put(sum,1);
+19                }
+20            }
+21        }
+22        for(int i=0;i<nums3.length;i++)
+23        {
+24            for(int j=0;j<nums4.length;j++)
+25            {
+26                int sum=nums3[i]+nums4[j];
+27                if(map.containsKey(-sum) )
+28                {
+29                    count+=map.get(-sum);
+30                }
+31            }
+32        }
+33        return count;
+34
+35    }
+36}
