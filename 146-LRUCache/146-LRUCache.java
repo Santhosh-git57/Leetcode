579e60e21b75523928490f7bce4dc7e602e7925d
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:16:07 +0530

    File updated at 8/27/2026, 9:16:07 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,37 +1,17 @@
-// Last updated: 8/27/2026, 9:14:05 AM
-1class Solution {
-2    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-3
-4        int count=0;
-5        HashMap<Integer,Integer>map=new HashMap<>();
-6        for(int i=0;i<nums1.length;i++)
-7        {
-8            for(int j=0;j<nums2.length;j++)
-9            {
-10                int sum=nums1[i]+nums2[j];
-11                if(map.containsKey(sum))
-12                {
-13                    int freq=map.get(sum);
-14                    map.put(sum,freq+1);
-15                }
-16                else
-17                {
-18                    map.put(sum,1);
-19                }
-20            }
-21        }
-22        for(int i=0;i<nums3.length;i++)
-23        {
-24            for(int j=0;j<nums4.length;j++)
-25            {
-26                int sum=nums3[i]+nums4[j];
-27                if(map.containsKey(-sum) )
-28                {
-29                    count+=map.get(-sum);
-30                }
-31            }
-32        }
-33        return count;
-34
-35    }
-36}
+// Last updated: 8/27/2026, 9:16:07 AM
+1public class Solution {
+2    public boolean repeatedSubstringPattern(String s) {
+3        int n = s.length();
+4        for (int i = 1; i <= n / 2; i++) {
+5            if (n % i == 0) {
+6                String substring = s.substring(0, i);
+7                StringBuilder repeated = new StringBuilder();
+8                for (int j = 0; j < n / i; j++) {
+9                    repeated.append(substring);
+10                }
+11                if (repeated.toString().equals(s)) return true;
+12            }
+13        }
+14        return false;
+15    }
+16}
