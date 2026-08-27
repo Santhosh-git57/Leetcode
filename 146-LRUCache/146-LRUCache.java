21858b7003c97dc1b2019b614549a694623e40f8
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:13:08 +0530

    File updated at 8/27/2026, 9:13:07 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,21 +1,17 @@
-// Last updated: 8/27/2026, 9:12:27 AM
+// Last updated: 8/27/2026, 9:13:07 AM
 1class Solution {
-2    public int arrangeCoins(int n) {
-3        int low=1;
-4        int high=n;
-5        int ans=1;
-6        while(low<=high){
-7            int mid=low+(high-low)/2;
-8            if(helper(mid)<=n){
-9                ans=mid;
-10                low=mid+1;
-11            }
-12            else high=mid-1;
-13        }
-14        return ans;
-15    }
-16    public long helper(int mid){
-17        long sum=(long)mid*(mid+1)/2;
-18        return sum;
-19    }
-20}
+2  public List<Integer> findDisappearedNumbers(int[] nums) {
+3    List<Integer> ans = new ArrayList<>();
+4
+5    for (final int num : nums) {
+6      final int index = Math.abs(num) - 1;
+7      nums[index] = -Math.abs(nums[index]);
+8    }
+9
+10    for (int i = 0; i < nums.length; ++i)
+11      if (nums[i] > 0)
+12        ans.add(i + 1);
+13
+14    return ans;
+15  }
+16}
