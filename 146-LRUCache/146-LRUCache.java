afd5ca7f196ca9e3e4fb47bdb964bb6d2e282a37
Santhosh <santhoshsk28677@gmail.com>
2026-08-27 09:11:11 +0530

    File updated at 8/27/2026, 9:11:10 AM

--- 146-LRUCache/146-LRUCache.java
+++ 146-LRUCache/146-LRUCache.java
@@ -1,22 +1,23 @@
-// Last updated: 8/27/2026, 9:10:08 AM
+// Last updated: 8/27/2026, 9:11:10 AM
 1class Solution {
-2    public List<String> fizzBuzz(int n) {
-3        List<String> ans = new ArrayList<>();
-4
-5        for(int i=1; i<=n; i++) {
-6            if(i%3 ==0 && i%5==0) {
-7                ans.add("FizzBuzz");
-8            }
-9            else if(i%3==0) {
-10                ans.add("Fizz");
-11            }
-12            else if(i%5==0) {
-13                ans.add("Buzz");
-14            }
-15            else {
-16                ans.add(Integer.toString(i));
-17            }
-18        }
-19        return ans;
-20    }
-21}
+2    public String addStrings(String num1, String num2) {
+3        StringBuilder sb=new StringBuilder();
+4        int i=num1.length()-1;
+5        int j=num2.length()-1;
+6        int carry=0;
+7        while(i>=0||j>=0||carry!=0){
+8            int sum=carry;
+9            if(i>=0){
+10                sum+=num1.charAt(i)-'0';
+11                i--;
+12            }
+13            if(j>=0){
+14                sum+=num2.charAt(j)-'0';
+15                j--;
+16            }
+17            sb.append(sum%10);
+18            carry=sum/10;
+19        }
+20       return sb.reverse().toString();
+21    } 
+22}
